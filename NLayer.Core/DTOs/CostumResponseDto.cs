96c5c7d36e6603d927cc7d06f2585abbb8b5ddc2
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-09-18 19:04:19 +0300

    add DTOs at Core Layer

---
+++ NLayer.Core/DTOs/CostumResponseDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public class CostumResponseDto<T>
+    {
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        public T Data { get; set; }
+
+        public List<String> Errors { get; set; }
+
+
+
+        public static CostumResponseDto<T> Success(int statusCode,T data)
+        {
+            return new CostumResponseDto<T> { StatusCode = statusCode, Data = data}; 
+        }
+        public static CostumResponseDto<T> Success(int statusCode)
+        {
+            return new CostumResponseDto<T> { StatusCode = statusCode};
+        }
+
+        public static CostumResponseDto<T> Fail(int statusCode, List<String> errors)
+        {
+            return new CostumResponseDto<T> { StatusCode = statusCode , Errors=errors };
+        }
+
+        public static CostumResponseDto<T> Fail(int statusCode, string error)
+        {
+            return new CostumResponseDto<T> { StatusCode = statusCode, Errors = new List<string> { error} };
+        }
+
+    }
+}
